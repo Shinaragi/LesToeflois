3034ce076e7c8b9cabd0859d3826f892125ae20c
MATHIS <mathis@jelmoni.fr>
2021-01-08 11:34:23 +0100

    First

---
+++ Casse Brique/Assets/Block.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        // Destroy the whole Block
+        Destroy(gameObject);
+    }
+}
